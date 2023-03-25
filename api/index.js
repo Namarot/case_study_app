28b46dea76b641fa08e54ad26874415eaf31263d
Utku Toraman <utkutoraman@gmail.com>
2023-03-25 18:35:49 +0300

    ci test

--- api/index.js
+++ api/index.js
@@ -16,7 +16,7 @@ app.use((req, res, next) => {
 
 //test route
 app.get('/', (req, res, next) => {
-  res.send('Hello World, version 2');
+  res.send('Hello World, version 3');
 });
 
 //CRUD routes
